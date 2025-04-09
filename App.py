064396f5ab37b2ef52eb7b0ce0a86148a96eaa57
Venkatramireddy <venkatramireddyvatrapu@gmail.com>
2025-04-09 18:09:18 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,11 +1,33 @@
-from flask import Flask
+import os
+import tensorflow as tf
+from flask import Flask, request, jsonify
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 
 app = Flask(__name__)
 
+# Load the trained model
+model = load_model('emnist_cnn_model.keras')
+
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Model API is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        img_file = request.files['image']
+        img = image.load_img(img_file, target_size=(28, 28), color_mode='grayscale')
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array /= 255.0
+
+        # Predict using the model
+        predictions = model.predict(img_array)
+        predicted_class = np.argmax(predictions)
+
+        return jsonify({'predicted_class': predicted_class})
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
+    app.run(debug=True)
