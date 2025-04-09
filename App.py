a6761ccfef80f1028dee25f483cffc3feadd872c
Venkatramireddy <venkatramireddyvatrapu@gmail.com>
2025-04-09 17:15:03 +0530

    Update App.py

--- App.py
+++ App.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def home():
     return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
