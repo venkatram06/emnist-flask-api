b38fd4d049a76be33bf8d20d2a4ac84ddac51412
Venkatramireddy <venkatramireddyvatrapu@gmail.com>
2025-04-09 17:04:59 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hello, World!")
+    return "Hello, World!"
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
